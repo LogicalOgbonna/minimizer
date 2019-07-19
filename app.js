fae62d093d291f6dd7ad110417e1d385572ef116
ARINZE OGBONNA <arinze.ogbonna.198717@unn.edu.ng>
2019-07-19 17:49:16 +0100

    ready now

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.use("/api/", indexRouter);
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("view/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "view", "build", "index.html"));
   });
 }
 const port = process.env.PORT || 2000;
