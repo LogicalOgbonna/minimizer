fb9dca5b5eb716a023497f97716ca2356c4f6a33
ARINZE OGBONNA <arinze.ogbonna.198717@unn.edu.ng>
2019-07-19 17:39:42 +0100

    ready now

--- app.js
+++ app.js
@@ -13,22 +13,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/", indexRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  // next(createError(404));
-});
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error  in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const port = process.env.PORT || 2000;
 
 app.listen(port, () => console.log(`App Running on Port ${port}`));
