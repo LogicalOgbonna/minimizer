31efb6fd4e3612059037dc9fc82509ed8d4390d5
ARINZE OGBONNA <arinze.ogbonna.198717@unn.edu.ng>
2019-08-17 17:45:06 +0100

    Fixed .lenght problem

--- view/src/components/Dashboard/Users/Company.js
+++ view/src/components/Dashboard/Users/Company.js
@@ -43,7 +43,7 @@ export default function Company({ graph }) {
                   </div>
                 </div>
               )}
-              {!graph && (
+              {graph && !graph.length && (
                 <div className="card-container mb-5">
                   <h3
                     // style={{ margin: "auto" }}
